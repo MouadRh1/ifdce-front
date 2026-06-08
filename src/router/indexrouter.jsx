8417c2faf3206989aa34876b535f426f20e62288
Mouad Rahmouni <mouadrah01@gmail.com>
2026-06-08 14:18:25 +0100

    correction de deploy

--- src/router/indexrouter.jsx
+++ src/router/indexrouter.jsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
-import Layout from "../component/layout";
+import Layout from "../component/Layout.jsx";
 import Home from "../page/home";
 import About from "../page/about";
 import Authentification from "../page/authentification";
@@ -12,8 +12,8 @@ import FieldDashboard from "../page/FieldDashboard";
 import Notfound from "../page/notfound.jsx";
 import AdminDashboard from "../component/AdminDashboard";
 import Contact from "../page/contact";
-import Technicien from '../page/Technicien'; // Add this import
-import TechnicienSuperieur  from "../page/TechnicienSuperieur.jsx"; 
+import Technicien from "../page/Technicien"; // Add this import
+import TechnicienSuperieur from "../page/TechnicienSuperieur.jsx";
 import Techaidecomptable from "../page/techaidecomptable.jsx"; // Add this import
 import LicenceProfessionnelle from "../page/LicenceProfessionnelle.jsx"; // Add this import
 import VAEPage from "../page/VAEPage.jsx"; // Add this import
@@ -21,95 +21,95 @@ import MasterProfessionnel from "../page/MasterProfessionnel.jsx"; // Add this i
 import Profile from "../page/Profile.jsx"; // Add this import
 
 export const router = createBrowserRouter([
-    {
-        element: <Layout />,
-        children: [
-            {
-                path: "/",
-                element: <Home />,
-            },
-            {
-                path: "/about",
-                element: <About />,
-            },
-            {
-                path: "/login",
-                element: <Login />,
-            },
-             {
-                path: "/admin-dashboard",
-                element: <AdminDashboard />,
-            },
-             {
-                path: "/Technicien",
-                element: <Technicien />,
-            },
-            {
-                path: "/authentification",
-                element: <Authentification />,
-            },
-             {
-                path: "/techaidecomptable",
-                element: <Techaidecomptable />,
-            },
-            {
-                path: "/TechnicienSuperieur",
-                element: <TechnicienSuperieur />,
-            },
-            {
-                path: "/LicenceProfessionnelle",
-                element: <LicenceProfessionnelle />,
-            },
-            {
-                path: "/VAEPage",
-                element: <VAEPage />,
-            },
-            {
-                path: "/contact",
-                element: <Contact />,
-            },
-            {
-                path: "/MasterProfessionnel",
-                element: <MasterProfessionnel />,
-            },
-            // Protected Admin Routes
-            {
-                path: "/diploma-dashboard",
-                element: (
-                    <AdminRoute>
-                        <DiplomaDashboard />
-                    </AdminRoute>
-                ),
-            },
-            {
-                path: "/field-dashboard",
-                element: (
-                    <AdminRoute>
-                        <FieldDashboard/>
-                    </AdminRoute>
-                ),
-            },
-            {
-                path: "/create-diploma",
-                element: (
-                    <AdminRoute>
-                        <CreateDiploma />
-                    </AdminRoute>
-                ),
-            },
-            {
-        path: '*',
-       element: <Notfound/>,
-       },
-            // Protected User Routes
-            {
-                path: "/profile",
-                element: (
-                    <ProtectedRoute>
-                        <Profile />
-                    </ProtectedRoute>
-                ),
-            },
-        ],
-    },
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/admin-dashboard",
+        element: <AdminDashboard />,
+      },
+      {
+        path: "/Technicien",
+        element: <Technicien />,
+      },
+      {
+        path: "/authentification",
+        element: <Authentification />,
+      },
+      {
+        path: "/techaidecomptable",
+        element: <Techaidecomptable />,
+      },
+      {
+        path: "/TechnicienSuperieur",
+        element: <TechnicienSuperieur />,
+      },
+      {
+        path: "/LicenceProfessionnelle",
+        element: <LicenceProfessionnelle />,
+      },
+      {
+        path: "/VAEPage",
+        element: <VAEPage />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/MasterProfessionnel",
+        element: <MasterProfessionnel />,
+      },
+      // Protected Admin Routes
+      {
+        path: "/diploma-dashboard",
+        element: (
+          <AdminRoute>
+            <DiplomaDashboard />
+          </AdminRoute>
+        ),
+      },
+      {
+        path: "/field-dashboard",
+        element: (
+          <AdminRoute>
+            <FieldDashboard />
+          </AdminRoute>
+        ),
+      },
+      {
+        path: "/create-diploma",
+        element: (
+          <AdminRoute>
+            <CreateDiploma />
+          </AdminRoute>
+        ),
+      },
+      {
+        path: "*",
+        element: <Notfound />,
+      },
+      // Protected User Routes
+      {
+        path: "/profile",
+        element: (
+          <ProtectedRoute>
+            <Profile />
+          </ProtectedRoute>
+        ),
+      },
+    ],
+  },
 ]);
